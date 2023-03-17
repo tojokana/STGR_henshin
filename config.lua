6d16278a2aa0b2d2df162e4951c675d3db5274bf
tojokana <osuke.karna@outlook.jp>
2023-03-17 12:20:23 +0900

    translation

--- config.lua
+++ config.lua
@@ -1,2 +1,14 @@
 -- 言語の設定
 language = "ja"
+
+return {
+    message = {
+        server_error = "サーバーエラーが発生しました。しばらくしてから再度お試しください。",
+        transformation_success = "変身に成功しました！",
+        transformation_failure = "変身に失敗しました。",
+        invalid_character = "不正なキャラクターです。",
+        invalid_transformation = "このキャラクターは変身できません。",
+        insufficient_mana = "魔力が不足しています。",
+        transformation_cost = "変身には%dmPの魔力が必要です。"
+    }
+}
---
+++ language/en.lua
@@ -0,0 +1,9 @@
+return {
+    server_error = "A server error occurred. Please try again later.",
+    transformation_success = "Transformation successful!",
+    transformation_failure = "Transformation failed.",
+    invalid_character = "Invalid character.",
+    invalid_transformation = "This character cannot transform.",
+    insufficient_mana = "Insufficient mana.",
+    transformation_cost = "Transformation requires %dmP of mana."
+}
