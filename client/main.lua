151dc404ef9f93383a33798c33681a50804b0a72
tojokana <osuke.karna@outlook.jp>
2023-03-17 14:42:19 +0900

    qbcore式

--- client/main.lua
+++ client/main.lua
@@ -1,28 +1,32 @@
-function onTransform()
-  local playerPed = GetPlayerPed(-1)
-  local modelHash = GetHashKey(modelName)
-  RequestModel(modelHash)
-  while not HasModelLoaded(modelHash) do
-    Citizen.Wait(100)
+RegisterCommand("henshin", function(source, args, rawCommand)
+  local model
+  local playerPed = PlayerPedId()
+  
+  if not args[1] then -- 引数がない場合、元のスキンに戻る
+    model = GetHashKey("mp_m_freemode_01")
+  elseif args[1] == "cat" then -- "cat" が引数の場合、 a_c_cat_01 のスキンに変更する
+    model = GetHashKey("a_c_cat_01")
+  elseif args[1] == "dog" then -- "dog" が引数の場合、 a_c_husky のスキンに変更する
+    model = GetHashKey("a_c_husky")
   end
-  SetPlayerModel(PlayerId(), modelHash)
-  SetModelAsNoLongerNeeded(modelHash)
-  TriggerEvent('skinchanger:getSkin', function(skin)
-    TriggerServerEvent('esx_skin:setLastSkin', skin)
-  end)
-  TriggerEvent('esx:onPlayerSpawn')
-end
-
--- Add command handler
-RegisterCommand('henshin', function(source, args)
-  if #args > 0 then
-    local modelName = args[1]
-    onTransform(modelName)
-  else
-    -- Default to a_c_cat_01 if no argument is provided
-    onTransform("a_c_cat_01")
+  
+  if model then
+    QBCore.Functions.SpawnCharacter(playerPed, { x = 0, y = 0, z = 0 }, function(spawnedPed)
+      SetPlayerModel(PlayerId(), model)
+      SetModelAsNoLongerNeeded(model)
+      FreezeEntityPosition(spawnedPed, true)
+      SetEntityInvincible(spawnedPed, true)
+      SetEntityVisible(spawnedPed, false, false)
+      SetTimeout(1000, function()
+        SetPlayerModel(PlayerId(), model)
+        FreezeEntityPosition(playerPed, false)
+        SetEntityInvincible(playerPed, false)
+        SetEntityVisible(playerPed, true, false)
+        SetModelAsNoLongerNeeded(model)
+      end)
+    end)
   end
-end,false)
+end, false)
 
 function DrawNotification(text)
   SetNotificationTextEntry("STRING")
